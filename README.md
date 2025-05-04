b76cd46ef8ede1d2a37b9622f49eff442150efb1
Denys <denmen133@gmail.com>
2025-05-04 20:04:51 +0300

    feat: added workflow.yml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TotalFitnesBooker
+# TotalFitnessBooker
+
+[![Book Interval Training](https://github.com/denzero13/total_fitness_reservation/actions/workflows/resrvation_7h.yml/badge.svg?event=schedule)](https://github.com/denzero13/total_fitness_reservation/actions/workflows/resrvation_7h.yml)
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 import time
 import os
 
+
 URL = "https://totalfitness-ua.perfectgym.com/ClientPortal2/"
-EMAIL = "denmen133@gmail.com"     # 🔁 Введи свій емейл
-PASSWORD = "qwertyden13"      # 🔁 Введи свій пароль
 
 
-def main(email: str, password: str):
-    driver = webdriver.Chrome()
+def main(email: str, password: str, training_with_coach: str):
+    options = Options()
+    options.binary_location = "/usr/bin/chromium-browser"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 15)
 
     driver.get(URL)
@@ -21,29 +28,34 @@ def main(email: str, password: str):
     wait.until(EC.element_to_be_clickable((By.ID, "confirm"))).click()
 
     time.sleep(5)
-    driver.get(f"{URL}#/Classes/11/List?date=2025-05-05")
+    today = datetime.today().strftime('%Y-%m-%d')
+    driver.get(f"{URL}#/Classes/11/List?date={today}")
 
     time.sleep(5)
     items = driver.find_elements(By.CSS_SELECTOR, 'cp\\:classes-class-item')
 
+    training, coach = training_with_coach.split(",")
+
     found = False
     for item in items:
         text = item.text.lower()
-        if "pilates" in text and "олена мікуліна" in text:
+
+        if training in text and coach in text:
             try:
                 book_now_button = WebDriverWait(item, 3).until(
                     EC.element_to_be_clickable((
                         By.XPATH,
-                        './/div[contains(@class, "cp-btn-classes-action") and .//span[text()="Взяти участь"]]'
+                        './/div[contains(@class, "cp-btn-classes-action") and .//span[text()="Book now"]]'
                     ))
                 )
                 book_now_button.click()
-                print("✅ Успішно заброньовано Pilates з Оленою Мікуліною")
+                print(f"✅ Успішно заброньовано {training} з {coach}")
                 found = True
                 time.sleep(5)
                 break
             except Exception as e:
-                print("🚫 Кнопка 'Взяти участь' неактивна або відсутня:", e)
+                print(f"🚫 Кнопка 'Взяти участь' неактивна або відсутня:", e)
+                print(text)
 
 
     if not found:
@@ -52,4 +64,4 @@ def main(email: str, password: str):
     driver.quit()
 
 if __name__ == "__main__":
-    main(os.environ.get('DENYS_EMAIL'), os.environ.get('DENYS_PASS'))
+    main(os.environ.get('EMAIL'), os.environ.get('PASSWORD'), os.environ.get("TRAINING_WITH_COACH", ""))
